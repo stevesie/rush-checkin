e7bdb72ddace6bb40c99078ef3aeb140d65081a8
stevesie <stevesie@Steves-MacBook-Pro.local>
2013-01-14 22:26:50 -0500

    initial commit

---
+++ config.php
@@ -0,0 +1,46 @@
+<?php
+	$core_extensions = array('silk_icons');
+
+	//echo getcwd();
+	$environment = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : "127.0.0.1";
+	
+	switch($environment) {
+		default:
+			$prefs = array(
+				"FILE_ROOT"   => "/opt/rush-checkin/",
+				"URL_ROOT"    => "/",
+				"MEDIA_ROOT"  => "/",
+				"SERVER_ROOT" => "http://127.0.0.1:9000/",
+				"DSN"         => "mysql://root:@localhost/dkecornell",
+				"MAIL"		  => false,
+				"ADS"		  => false,
+				"TRACKING"    => false,
+				"DEFAULT"	  => "pages"
+			);
+			break;
+		case "dkecornell.com":
+			header("Location: http://www.dkecornell.com");
+			exit;
+		case "www.dkecornell.com":
+			$prefs = array(
+				"FILE_ROOT"   => "/opt/dkecornell/",
+				"URL_ROOT"    => "/",
+				"MEDIA_ROOT"  => "/",
+				"SERVER_ROOT" => "http://www.dkecornell.com/",
+				"DSN"         => "mysql://root:@localhost/dkecornell",
+				"MAIL"		  => false,
+				"ADS"		  => false,
+				"TRACKING"    => false,
+				"DEFAULT"	  => "pages"
+			);
+			break;
+	}
+	
+	include($prefs['FILE_ROOT'].'core/src/Core.php');
+	foreach($core_extensions as $name)
+		include($prefs['FILE_ROOT'].'core/ext/'.$name.'.php');
+	
+	$routes = array();
+	
+	$core = Core::instance($prefs, $routes);
+?>
